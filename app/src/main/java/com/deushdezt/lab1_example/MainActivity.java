b03053a87213a46b552003c3132b038bb607e3e5
DouglasHdezT <00004516@uca.edu.sv>
2019-02-11 23:15:27 -0600

    Agregando comentario

--- app/src/main/java/com/deushdezt/lab1_example/MainActivity.java
+++ app/src/main/java/com/deushdezt/lab1_example/MainActivity.java
@@ -3,6 +3,12 @@ package com.deushdezt.lab1_example;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/*
+* Estudiante: Douglas Antonio Hernandez Torres
+* Carnet: 00004516
+* Curso: Programacion de dispositivos moviles
+* Ciclo: 01/13
+* */
 public class MainActivity extends AppCompatActivity {
 
     @Override
